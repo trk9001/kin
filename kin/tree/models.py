01b21f8591f6d0c42bfffc7fc17287f09dece973
trk <dev.trk.9001@gmail.com>
2019-02-28 20:56:33 +0600

    Write a Kin model to represent family members

--- kin/tree/models.py
+++ kin/tree/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Kin(models.Model):
+    parents = models.ManyToManyField(
+        'self',
+        blank=True,
+        related_name='children',
+        related_query_name='child',
+        symmetrical=False,
+    )
+    name = models.CharField(
+        max_length=200,
+    )
