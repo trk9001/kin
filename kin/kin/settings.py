3aebbb2d73300991a91c1c522d30f4d2bd4964f5
trk <dev.trk.9001@gmail.com>
2019-03-01 12:27:04 +0600

    Change TIME_ZONE setting to UTC
    See (https://github.com/newvem/pytz).

--- kin/kin/settings.py
+++ kin/kin/settings.py
@@ -94,7 +94,7 @@ MEDIA_URL = '/media/'
 
 # Internationalization/localization settings
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Dhaka'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
